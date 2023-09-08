8e49eb51ff812104b54a84a9110e58b0e670d817
epasquie <epasquie@debian-epasquie.home>
2023-09-08 11:46:52 +0200

    update code

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ load_dotenv()
 # Récupère les variables
 FILE_EMBEDING = os.getenv("FILE_EMBEDING")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-QUESTION = "Est ce que la Camera AI Bullet de Ubiquiti est waterproof ? "
+VERBOSE=True
+QUESTION = "Quel est le vrai prenom de laylow ?"
 NUM_LINK = 3
-
 prompt = """
   Repond en français , essaie de faire une reponse complete:
   """+QUESTION
 
+def printdebug(text):
+  if VERBOSE:
+    print(text)
 
 def get_webpage_text(url):
   text_content = ""
@@ -33,23 +35,23 @@ def get_webpage_text(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     for tag in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'a', 'span']):
       text_content += tag.get_text() + "\n"
-    print(url+" est reussie")
+    printdebug(url+" est reussie")
   except :
     text_content = ""
-    print(url+" est un echec")
+    printdebug(url+" est un echec")
   return text_content
 
 def analyze_text(prompt):
     raw_documents = TextLoader(FILE_EMBEDING).load()
-    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     pages = text_splitter.split_documents(raw_documents)
-    text_splitter = TokenTextSplitter(chunk_size=500, chunk_overlap=0)
+    text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(pages)
     index = VectorstoreIndexCreator().from_documents(texts)
-    return index.query(prompt, llm=ChatOpenAI(model="gpt-3.5-turbo-16k",temperature=0.4))
+    return index.query(prompt, llm=ChatOpenAI(model="gpt-3.5-turbo-16k",temperature=0))
 
 def multi_traitement_ressource(prompt,data):
-    print("start embeding")
+    printdebug("start embeding")
     with open(FILE_EMBEDING, 'w', encoding='utf-8') as file:
         file.write(str(data))
     result = analyze_text(prompt)
@@ -66,11 +68,11 @@ def web_qa(url_list, query):
   content_ressource = ""
   for i in url_list:
     url = clean_url(i)
-    print('loading url : ',str(url))
+    printdebug('loading url : '+str(url))
     content_ressource += get_webpage_text(url)
 
   reponse = multi_traitement_ressource(query,content_ressource)
-  print("")
+  print("\nReponse :")
   print(reponse)
 
 def trouver_urls(texte):
